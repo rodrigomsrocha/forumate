e31675e7aa06a57065bb4fb3cf22d1b391d9255b
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-04-23 20:21:41 -0300

    paginate get all discussions routes

--- server/src/controllers/discussionsController.ts
+++ server/src/controllers/discussionsController.ts
@@ -4,12 +4,30 @@ import { v4 as uuid } from "uuid";
 import { z } from "zod";
 import { client } from "../lib/fauna";
 
-interface AuthMiddlewareRequest extends FastifyRequest {
+interface DiscussionsQuery {
+  after?: {
+    id: string;
+  }[];
+  data: {
+    data: {
+      id: string;
+      title: string;
+      content: string;
+      userId: string;
+    };
+    ts: number;
+    ref: {
+      id: string;
+    };
+  }[];
+}
+
+interface CreateDiscussionRequest extends FastifyRequest {
   userID?: string;
 }
 
 export async function createDiscussion(
-  req: AuthMiddlewareRequest,
+  req: CreateDiscussionRequest,
   reply: FastifyReply
 ) {
   const createDiscussionBody = z.object({
@@ -35,6 +53,43 @@ export async function createDiscussion(
     );
 
     return reply.send(newDiscussion);
+  } catch (error) {
+    return reply.send(error);
+  }
+}
+
+export async function getAllDiscussions(
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
+  const getAllDiscussionsQuery = z.object({
+    after: z.string().optional(),
+  });
+  const { after } = getAllDiscussionsQuery.parse(req.query);
+
+  try {
+    const response = await client.query<DiscussionsQuery>(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection("discussions")), {
+          size: 6,
+          ...(after && { after: q.Ref(q.Collection("discussions"), after) }),
+        }),
+        q.Lambda("X", q.Get(q.Var("X")))
+      )
+    );
+
+    const discussions = response.data.map((discussion) => {
+      return {
+        ...discussion.data,
+        ts: discussion.ts,
+        id: discussion.ref.id,
+      };
+    });
+
+    return reply.send({
+      after: response.after ? response.after[0].id : null,
+      discussions,
+    });
   } catch (error) {
     return reply.send(error);
   }
--- server/src/routes/discussionRoutes.ts
+++ server/src/routes/discussionRoutes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
-import { createDiscussion } from "../controllers/discussionsController";
+import {
+  createDiscussion,
+  getAllDiscussions,
+} from "../controllers/discussionsController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 export async function discussionsRoutes(app: FastifyInstance) {
   app.post("/discussions", { preValidation: authMiddleware }, createDiscussion);
+  app.get("/discussions", getAllDiscussions);
 }
